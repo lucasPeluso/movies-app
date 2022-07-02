ba05d15b09a2b171e688d58c7d4103c4ffc58c7c
Lucas Peluso <lucaspelusoweb@gmail.com>
2022-07-02 12:43:22 -0300

    Features

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./components/MovieCard";
 import './App.css';
 import SearchIcon from './search.svg'
 
-const API_URL = 'http://www.omdbapi.com?apikey=cfa20627';
+const API_URL = 'https://www.omdbapi.com?apikey=cfa20627';
 
 const App = () => {
 
